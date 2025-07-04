61635ac6ef7fa260b3960c7950efe37eaacbbc27
AnticsMaster <thomas.chiang4@gmail.com>
2025-07-04 11:43:33 +0800

    schema update
    added projects table and foreign keys for bugreport

--- webprog_group_quiz/schema.sql
+++ webprog_group_quiz/schema.sql
@@ -1,7 +1,15 @@
 CREATE TABLE IF NOT EXISTS bugreport (
     bug_id INTEGER PRIMARY KEY AUTOINCREMENT,
     developer_id INTEGER NOT NULL,
+    project_id INTEGER NOT NULL,
     bug_description TEXT NOT NULL,
     bug_severity TEXT NOT NULL,
-    report_time TEXT DEFAULT CURRENT_TIMESTAMP
+    report_time TEXT DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (project_id) REFERENCES projects(project_id)
+);
+
+CREATE TABLE IF NOT EXISTS projects (
+    project_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    project_name TEXT NOT NULL,
+    project_description TEXT NOT NULL
 );
